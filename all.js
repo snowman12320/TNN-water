34ce25759a05deef76303725694981aad5221d22
william <101874444+snowman12320@users.noreply.github.com>
2022-11-22 12:02:22 +0800

    done : index layout

---
+++ all.js
@@ -0,0 +1,37 @@
+// four_box
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 700,
+    arrows: false,
+    dots: false,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+});
+
+// go to top
+// call to action
+function showArrow() {
+  if ($(this).scrollTop() < 500) {
+    $("#arrow_up").hide();
+  } else {
+    $("#arrow_up").show();
+  }
+}
+$(window).scroll(showArrow);
